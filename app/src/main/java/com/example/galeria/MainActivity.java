d188e6618d1cf3302ed141d0aa356ffe0be23afd
raul96960 <raul96.960@gmail.com>
2020-06-16 00:05:06 -0500

    comentario

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+//comentario
 public class MainActivity extends AppCompatActivity {
 
 
